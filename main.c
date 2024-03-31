06af4f4b3021f6c9a02e814da5d4084c3d2a8e8c
Prog-Smiler <165482685+Prog-Smiler@users.noreply.github.com>
2024-03-31 13:16:59 +0545

    Add files via upload

---
+++ main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include<conio.h>
+int main(){
+	float a, b, sum, difference, product, divison;
+	char sign;
+	printf("Enter the first number: ");
+	scanf("%f", &a);
+	
+
+	printf("Enter the second number: ");
+	scanf("%f", &b);
+
+	sum = a + b;
+	printf("Sum: %f", sum);
+
+	difference = a - b;
+	printf("\nDiffrence: %f", difference);
+	
+
+	product = a * b;
+	printf("\nProduct: %f", product);
+
+	divison = a / b;
+	printf("\nDivison: %f", divison);
+	
+	return 0;
+}
